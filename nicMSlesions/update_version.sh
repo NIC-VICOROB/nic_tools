e8a77141a3452a4385ab74e6e3550d38bd48f4b6
Sergi <svalverde@eia.udg.edu>
2018-05-16 11:35:57 +0200

    update the docker without running the toolbox

---
+++ nicMSlesions/update_version.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# --------------------------------------------------
+# nicMSlesions
+#
+# Sergi Valverde 2018
+# https://github.com/sergivalverde/nicMSlesions
+#
+# --------------------------------------------------
+
+echo "updating the Docker image"
+docker pull nicvicorob/mslesions:latest
